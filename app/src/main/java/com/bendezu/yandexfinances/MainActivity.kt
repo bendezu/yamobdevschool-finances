aa6bac9badf010afe40a532c753b716907657c81
bendezu <bendezuvlad@gmail.com>
2018-07-20 19:40:00 +0300

    Add files to gitignore; modify build.gradle file

--- app/src/main/java/com/bendezu/yandexfinances/MainActivity.kt
+++ app/src/main/java/com/bendezu/yandexfinances/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scrolling)
+        setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
